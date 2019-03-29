c678ee194f1d610beca29c90c66bbb474d49b5e8
Jedakiah Moore <jedakiahmoore@gmail.com>
2019-03-28 19:49:58 -0500

    adding starting project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+
+function closeCookieAlert() {
+  var cookieAlert = '';
+
+  // close alert when 'close' button is pressed
+  // code goes here
+  console.log('cookie alert closed');
+
+}
